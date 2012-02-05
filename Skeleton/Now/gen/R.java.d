b3bc049d258ed6e2331e23bc296d26cc462867bf
Francisco Souza <f@souza.cc>
2012-02-05 01:19:38 -0200

    Added the Skeleton app (first example)

---
+++ Skeleton/Now/gen/R.java.d
@@ -0,0 +1,4 @@
+/Users/francisco.souza/javacode/android/Skeleton/Now/gen/com/francisco/android/skeleton/R.java \
+ : /Users/francisco.souza/javacode/android/Skeleton/Now/res/layout/main.xml \
+/Users/francisco.souza/javacode/android/Skeleton/Now/res/values/strings.xml \
+/Users/francisco.souza/javacode/android/Skeleton/Now/AndroidManifest.xml \
